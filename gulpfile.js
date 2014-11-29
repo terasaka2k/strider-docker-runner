554c6f2da4f92d2a86be4af480ed831cfc6af13f
Kinya TERASAKA <kinya.terasaka@visasq.com>
2014-11-30 08:52:41 +0900

    Use ECMAScript 6 and 7 polyfills

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,6 @@ const plumber = require('gulp-plumber');
 const to6 = require('gulp-6to5');
 const filter = require('gulp-filter');
 const cached = require('gulp-cached');
-const clean = require('gulp-clean');
 const filelog = require('gulp-filelog');
 const wrapper = require('gulp-wrapper');
 
@@ -18,7 +17,9 @@ const rmFiles = files.concat([
   'config', 'static', 'lib'
 ]);
 
-gulp.task('build', function() {
+gulp.task('build', ['compile/src', 'download/es7polyfill']);
+
+gulp.task('compile/src', function() {
   const jsFilter = filter('**/*.js');
 
   if (~this.seq.indexOf('watch')) {
@@ -50,13 +51,36 @@ gulp.task('build', function() {
     .pipe(gulp.dest('.'));
 });
 
+gulp.task('download/es7polyfill', function() {
+  const url6 = 'https://raw.githubusercontent.com/inexorabletash/polyfill/master/es6.js';
+  const url7 = 'https://raw.githubusercontent.com/inexorabletash/polyfill/master/experimental/es7.js';
+  const es7polyfillName = 'es7polyfill.js';
+
+  const literal = require('string-to-stream');
+  const series = require('stream-series');
+  const request = require('request');
+  const source = require('vinyl-source-stream');
+
+  return series([
+    literal('/* FETCHED FROM inexorabletash/polyfill/master/{es6,experimental/es7}.js on GitHub */\n\n'),
+    literal('(function() {\n'),
+    request(url6),
+    request(url7),
+    literal('}).call(global)')
+  ])
+    .pipe(source(es7polyfillName))
+    .pipe(filelog())
+    .pipe(gulp.dest('./lib/'));
+});
+
 gulp.task('watch', function() {
-  gulp.watch('src/**/*', ['build']);
+  gulp.watch('src/**/*', ['compile/src']);
 });
 
 gulp.task('clean', function() {
-  return gulp.src(rmFiles, {read: false})
-    .pipe(clean());
+  const rimraf = require('gulp-rimraf');
+  return gulp.src(rmFiles, { read: false })
+    .pipe(rimraf());
 });
 
-gulp.task('default', ['watch', 'build']);
+gulp.task('default', ['watch', 'compile/src']);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 /* eslint-env node */
 
-require('6to5/polyfill');
 require('6to5/runtime');
+require('./lib/es7polyfill');
+
+console.assert(Symbol.iterator, 'Ensure ECMAScript 6 Polyfill');
+console.assert([1, 2, 3].includes(1), 'Ensure ECMAScript 7 Polyfill');
 
 try {
   require('./lib/_development');
-} catch (err) {}
+} catch (err) {
+  if (err.code !== 'MODULE_NOT_FOUND') {
+    throw err;
+  }
+}
 
 module.exports = require('./lib');
